a898eeb395c6e030bcf90de8e2cf29a44cf88b40
Vinícius Aguiar <vinicius.aguiar@empiricus.com.br>
2022-01-16 17:25:14 -0300

    refactor: changed based url to heroku docker image deployed app

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,14 @@
+import pathlib
+
+from src.cornershop.set_up import IntegrationSetup
 from src.cornershop.utils.api import APIEnum
 
+csv_downloaded_dir = 'assets'
+parent_dir = pathlib.Path(__file__).parent
+i = IntegrationSetup()
+
 CREDENTIALS = {
-    'base_url': 'http://0.0.0.0:5000/',
+    'base_url': 'https://damp-mountain-80499.herokuapp.com/',
     'client_id': 'mRkZGFjM',
     'client_secret': 'ZGVmMjMz',
     'grant_type': 'client_credentials'
